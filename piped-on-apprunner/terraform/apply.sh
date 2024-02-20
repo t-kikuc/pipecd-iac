b8a864471c7eab35caa9e291e5a4f7a850b075bb
t-kikuc <tkikuchi07f@gmail.com>
2024-02-20 10:06:46 +0900

    Put together two apply commands

--- piped-on-apprunner/terraform/apply.sh
+++ piped-on-apprunner/terraform/apply.sh
@@ -4,12 +4,11 @@
 VAR_FILE=$1
 
 # FIXME: Ideally, we want to complete the deployment by `terraform apply` only once.
+# [1] Create pre-requisite resources to avoid error when creating the App Runner service.
+#    1. Create an ECR repository
+#    2. Push a piped image to it
+#    3. Create an IAM role for ECR access:  to avoid 'InvalidRequestException: Error in assuming access role' when creating the App Runner service.
+terraform apply -var-file=$VAR_FILE -target=aws_ecr_repository.ecr_piped_repo --target=null_resource.push_image --target=aws_iam_role.piped_ecr_access_role
 
-# [1] Create an ECR repository and push a piped image to it so that the AppRunner service can use it.
-terraform apply -var-file=$VAR_FILE -target=aws_ecr_repository.ecr_piped_repo --target=null_resource.push_image
-
-# [2] Create an IAM role for ECR access in advance to avoid 'InvalidRequestException: Error in assuming access role'
-terraform apply -var-file=$VAR_FILE --target=aws_iam_role.piped_ecr_access_role
-
-# [3] Create other resources
+# [2] Create other resources, including App Runner
 terraform apply -var-file=$VAR_FILE
